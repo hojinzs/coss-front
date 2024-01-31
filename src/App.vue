c7c55ef7ef2fea956fee7940ed1c0a690c593d98
stevelee <hojinzs@gmail.com>
2024-02-01 00:08:41 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,103 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import { gsap } from 'gsap'
+
+const logoElement = ref(null)
+
+onMounted(() => {
+  gsap.to(logoElement.value, {
+    scrollTrigger: logoElement.value,
+    y: -50,
+    yoyo: true,
+    duration: 0.5, // 애니메이션 지속 시간
+    repeat: -1, // 무한 반복
+    ease: 'power1.inOut', // 이징 함수 설정
+  });
+})
+
+</script>
+
+<template>
+  <header>
+    <img ref="logoElement" id="logoElement" alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+
+  a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+
+    &:first-of-type {
+      border: 0;
+    }
+
+    .router-link-exact-active {
+      color: var(--color-text);
+    }
+
+    .router-link-exact-active:hover {
+      background-color: transparent;
+    }
+
+  }
+}
+
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,24 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+// fontawesome setup
+// ref: https://fontawesome.com/v5/docs/web/use-with/vue/add-icons
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+
+library.add(faUserSecret)
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')
